5f1851a54d29ad384b1e111f1cff2d420c89894c
Sproff <sproff.oluwaseun@gmail.com>
2021-02-10 19:30:47 +0100

    added cors to the integration

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
+
 const app = express();
+app.use(cors());
+
 const routeRouter = require('./route');
 const port = process.env.PORT || 9000;
 
 app.use(express.json());
 app.use('/', routeRouter);
 
-mongoose.set('useCreateIndex', true);
-
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+    useCreateIndex: true,
   })
   .then(() => {
     app.listen(port, () => {
       console.log(`Server is running at ${port}`);
     });
-    console.log('DB Connected');
+    console.log('DB Connected Successfully');
   });
